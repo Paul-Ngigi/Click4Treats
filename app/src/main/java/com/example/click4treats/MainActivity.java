e18e635f8d4b3ed0691b8c71ab631e6e9baab384
Paul-Ngigi <paulkush7777@gmail.com>
2022-05-01 14:02:23 +0300

    Add a toast on a click event on the button

--- app/src/main/java/com/example/click4treats/MainActivity.java
+++ app/src/main/java/com/example/click4treats/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.click4treats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -14,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsBtn);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Loading...\nProcessing available restaurants for you",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
