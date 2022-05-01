3e544db3953c1eaa814db6803ba3b260f8faedc8
Paul-Ngigi <paulkush7777@gmail.com>
2022-05-01 15:51:04 +0300

    Take user input and log it

--- app/src/main/java/com/example/click4treats/MainActivity.java
+++ app/src/main/java/com/example/click4treats/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsBtn);
+        mLocationEditText = (EditText)findViewById(R.id.locationEditTxt);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Toast.makeText(MainActivity.this, "Loading...\nProcessing available restaurants for you",
 //                        Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 startActivity(intent);
             }
