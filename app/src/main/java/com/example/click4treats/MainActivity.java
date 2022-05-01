9c2749a1df8d948af97d96b7eaf84aefdeace2e8
Paul-Ngigi <paulkush7777@gmail.com>
2022-05-01 13:56:12 +0300

    create a local variable for holding the `find restaurants button` and use the button's id to fetch it

--- app/src/main/java/com/example/click4treats/MainActivity.java
+++ app/src/main/java/com/example/click4treats/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.click4treats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsBtn);
     }
 }
